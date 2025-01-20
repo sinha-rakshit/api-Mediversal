02cef1d81f5a58dbf5d568fd255a9b56bb9087b0
sinha-rakshit <sinha.rakshit.16@gmail.com>
2025-01-21 01:40:14 +0530

    initialized and added logger

---
+++ src/config/logger.js
@@ -0,0 +1,25 @@
+const winston = require('winston');
+
+const logFormat = winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json() // Ensure logs are formatted in JSON
+);
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: logFormat,
+    transports: [
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.json()
+        )
+      }),
+  
+      new winston.transports.File({ filename : "src/logs/error.log", level : "error"}),
+      new winston.transports.File({filename : "src/logs/combine.log"})
+    ]
+  });
+
+  module.exports = logger;
